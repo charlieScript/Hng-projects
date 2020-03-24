a67ae6ac327f0fddf0bb683cd114a59e1f6a190f
charlieScript <31950467+charlieScript@users.noreply.github.com>
2020-03-24 10:28:35 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,13 @@
+let name = "Onedibe Chigozie Charles";
+
+let courses = ["HTML", "CSS", "JAVASCRIPT"];
+
+if ((courses.length % 2) === 0) {
+    console.log("EVEN");
+} else {
+    console.log("ODD");
+    for (let i = 1; i <= 200; i+= 2) {
+        console.log(i + "<br/>");
+    }
+}
+
