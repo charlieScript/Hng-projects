bb1444b72b9d761d7a20386bcfa37507214bc350
charlieScript <31950467+charlieScript@users.noreply.github.com>
2020-04-01 14:14:11 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -1,13 +1,57 @@
-let name = "Onedibe Chigozie Charles";
+let data = [
+    {principal: 2500, time: 1.8},
+    {principal: 1000, time: 5},
+    {principal: 3000, time: 1},
+    {principal: 2000, time: 3}
+];
 
-let courses = ["HTML", "CSS", "JAVASCRIPT"];
-
-if ((courses.length % 2) === 0) {
-    console.log("EVEN");
-} else {
-    console.log("ODD");
-    for (let i = 1; i <= 200; i+= 2) {
-        console.log(i + "<br/>");
-    }
+function interestCalculator(data) {
+    let interestData = [];
+    data.forEach(i => {
+        if (i.principal >= 2500 && (i.time > 1 && i.time < 3 )) {
+            let rate = 3;
+            let interest;
+            interest = (i.principal * i.time * rate) / 100;
+            interestData.push({
+                principal: i.principal,
+                time: i.time,
+                rate: rate,
+                interest: interest
+            })
+        } else if (i.principal >= 2500 && i.time < 3) {
+            let rate = 4;
+            let interest;
+            interest = (i.principal * i.time * rate) / 100;
+            interestData.push({
+              principal: i.principal,
+              time: i.time,
+              rate: rate,
+              interest: interest
+            });
+        } else if (i.principal < 2500 && i.time <= 1) {
+            let rate = 2;
+            let interest;
+            interest = (i.principal * i.time * rate) / 100;
+            interestData.push({
+              principal: i.principal,
+              time: i.time,
+              rate: rate,
+              interest: interest
+            });
+        } else {
+            let rate = 1;
+            let interest;
+            interest = (i.principal * i.time * rate) / 100;
+            interestData.push({
+              principal: i.principal,
+              time: i.time,
+              rate: rate,
+              interest: interest
+            });
+        } 
+    });
+    console.log(interestData);
+    return interestData;
 }
 
+interestCalculator(data)
